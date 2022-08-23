a2756fb5d1bf49d6a81d7e719021a82f1750ea52
AnitaLuzardo <aniebratt@gmail.com>
2022-08-22 23:50:32 -0300

    configura base de datos

---
+++ database/config/config.js
@@ -0,0 +1,23 @@
+module.export= {
+  "development": {
+    "username": "planet_cookie",
+    "password": "zaq12wsx",
+    "database": "planet_cookie",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "planet_cookie",
+    "password": "zaq12wsx",
+    "database": "planet_cookie",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "planet_cookie",
+    "password": "zaq12wsx",
+    "database": "planet_cookie",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
