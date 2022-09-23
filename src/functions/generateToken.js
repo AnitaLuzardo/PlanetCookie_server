af2c06ad1ab68f646a3d8838f2b86cba168cc229
AnitaLuzardo <aniebratt@gmail.com>
2022-09-22 23:08:35 -0300

    crea variables de entorno

--- src/functions/generateToken.js
+++ src/functions/generateToken.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (id) => {
-  const expiration = '15d';
+  const expiration = process.env.JWT_TIEMPO_EXPIRA;
 
-  return jwt.sign({ id }, 'secret', {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: expiration
   });
 };
---
+++ src/middleware/authentication.js
@@ -0,0 +1,2 @@
+const jwt = require('jsonwebtoken');
+const database = ('../../database/models')
