b176012409393e119b814c88b3880dd01fd41eb3
AnitaLuzardo <aniebratt@gmail.com>
2022-10-03 21:05:07 -0300

    usando cors

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ require('dotenv').config({path:'./.env'});
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended:false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended:true }));
 
 const routes = require ('./src/routes/apiRouter');
 
